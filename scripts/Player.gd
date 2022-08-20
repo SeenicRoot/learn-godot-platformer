35132c2a33e0e94b2f5d78211a49762e57e033a7
Zi Lun Li <zilunli@hotmail.com>
2022-08-20 15:20:41 -0400

    Fixed Player Movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
 var movement_speed : int = 300
-var crouching_speed : int = 50
+var is_ducking : bool = false
+var ducking_speed : int = 100
+var is_airborne : bool = false
 var jump_force : int = 600
 var gravity : int = 800
 
@@ -13,44 +15,65 @@ onready var animated_sprite : AnimatedSprite = get_node("AnimatedSprite")
 func _physics_process(delta):
 
 	velocity.x = 0
-	#crouch
-	if Input.is_action_pressed("duck"):
-		animated_sprite.animation = "duck"
-	if Input.is_action_pressed("duck") and Input.is_action_pressed("move_left"):
-		animated_sprite.animation = "duck"
-		velocity.x -= crouching_speed
-	if Input.is_action_pressed("duck") and Input.is_action_pressed("move_right"):
-		animated_sprite.animation = "duck"
-		velocity.x += crouching_speed
-	if Input.is_action_pressed("duck"):
-		animated_sprite.animation = "duck"
-	if Input.is_action_just_released("duck"):
-		animated_sprite.animation = "stand"
-	if Input.is_action_pressed("move_left"):
-		animated_sprite.animation = "walk"
-		velocity.x -= movement_speed
-	if Input.is_action_just_released("move_left"):
-		animated_sprite.animation = "stand"
-		velocity.x = 0
-	if Input.is_action_pressed("move_right"):
-		animated_sprite.animation = "walk"
-		velocity.x += movement_speed
-	if Input.is_action_just_released("move_right"):
-		animated_sprite.animation = "stand"
-		velocity.x = 0
-		
+	
+	#movement func
+	_move_left()
+	_move_right()
+	_jump()
+	_duck()
+	
 	#applying velocity
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-#	#applying gravity (delta is time duration between each frame (1/60))
+	#applying gravity (delta is time duration between each frame (1/60))
 	velocity.y += gravity * delta
-#
-#	#jump key
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y -= jump_force
-		
 	#sprite direction
 	if velocity.x < 0:
 		animated_sprite.flip_h = true
 	elif velocity.x > 0:
 		animated_sprite.flip_h = false
+	
+
+func _move_left():
+	if Input.is_action_pressed("move_left"):
+		animated_sprite.animation = "walk"
+		velocity.x -= movement_speed
+		if is_ducking:
+			velocity.x = 0
+			velocity.x -= ducking_speed
+		elif is_airborne:
+			animated_sprite.animation = "stand"
+	elif Input.is_action_just_released("move_left"):
+		animated_sprite.animation = "stand"
+		velocity.x = 0
+	
+
+func _move_right():
+	if Input.is_action_pressed("move_right"):
+		animated_sprite.animation = "walk"
+		velocity.x += movement_speed
+		if is_ducking:
+			velocity.x = 0
+			velocity.x += ducking_speed
+		elif is_airborne:
+			animated_sprite.animation = "stand"
+	elif Input.is_action_just_released("move_right"):
+		animated_sprite.animation = "stand"
+		velocity.x = 0
+	
+
+func _jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y -= jump_force
+		is_airborne = true
+		animated_sprite.animation = "stand"
+	
+
+func _duck():
+	if Input.is_action_pressed("duck"):
+		animated_sprite.animation = "duck"
+		is_ducking = true
+
+	elif Input.is_action_just_released("duck"):
+		animated_sprite.animation = "stand"
+		is_ducking = false
+	
